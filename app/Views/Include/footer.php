0859cef5a41c0dd69ea6faa8373413990bfe62ed
Your Name <your.email@example.com>
2025-09-03 16:42:00 +0300

    Better header and footer

--- app/Views/Include/footer.php
+++ app/Views/Include/footer.php
@@ -2,6 +2,6 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.4.0
     </div>
-    <strong>Copyright &copy; 2020-2021 Tejas Chavda.</strong> All rights
+    <strong>Copyright &copy; 2025; EMAX SUPPLIES & LOGISTICS LIMITED.</strong> All rights
     reserved.
   </footer>
